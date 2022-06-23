6bb7cccd536f7077de0e3dd4a863623a1faf58a0
Shudhanshu <sudhanshubaaghi@gmail.com>
2022-06-23 20:45:38 +0530

    added dispatch

--- src/index.js
+++ src/index.js
@@ -9,7 +9,14 @@ import movies from './reducers';
 
 const store = createStore(movies);
 console.log('store', store);
-console.log('State', store.getState());
+console.log('Before State', store.getState());
+
+store.dispatch({
+  type: 'ADD_MOVIES',
+  movies: [ {name: 'Superman'} ]
+})
+
+console.log('After State', store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
